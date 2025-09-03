f8041a24cc8ff41d70e490caa369d8466dd7dcb7
UdaraAthauda <athaudaprasath@gmail.com>
2025-09-03 12:18:17 +0530

    add items to the cart -> frontend

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,22 +1,27 @@
 import {
-  Box,
   Container,
-  Heading,
   SimpleGrid,
   Text,
   Card,
   Image,
   Button,
+  NumberInput,
 } from "@chakra-ui/react";
+import { GiHealthIncrease } from "react-icons/gi";
 import { URL, COMPANY_NAME, COMPANY_ID } from "@/constants";
 import Hero from "@/components/Hero";
 import api from "@/api";
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { toaster } from "@/components/ui/toaster";
 
 export default function Home() {
   const [products, setProducts] = useState([]);
-  const navigate = useNavigate()
+  const [companyID, setCompanyID] = useState(null)
+  const navigate = useNavigate();
+  
+  // store quantities by product.id
+  const [quantities, setQuantities] = useState({});
 
   useEffect(() => {
     const getData = async () => {
@@ -25,15 +30,15 @@ export default function Home() {
         setProducts(res.data);
 
         const res2 = await api.get("company/");
-        
+
         if (res2.data && res2.data.length > 0) {
-          const company = res2.data[0]
-          localStorage.setItem(COMPANY_ID, company.id)
-          localStorage.setItem(COMPANY_NAME, company.name)
+          const company = res2.data[0];
+          setCompanyID(company.id)
+          localStorage.setItem(COMPANY_ID, company.id);
+          localStorage.setItem(COMPANY_NAME, company.name);
         } else {
-          navigate('/company')
+          navigate("/company");
         }
-        
       } catch (error) {
         console.error("error in product fetching: ", error);
       }
@@ -42,13 +47,45 @@ export default function Home() {
     getData();
   }, []);
 
+  const handleFormChange = (productID, details) => {
+    setQuantities(prev => ({...prev, [productID]: parseInt(details.value) || 1}))
+  };
+
+  const handleSubmit = async (e, productID) => {
+    e.preventDefault();
+
+    const data = {
+      product_id: productID,
+      quantity: quantities[productID] || 1,
+    };
+    
+    try {
+      const res = await api.post(`cart/add/${companyID}/`, data)
+     
+      toaster.create({
+          title: "Successful",
+          description: "Item is added to the cart.",
+          type: 'success',
+          duration: 5000,
+        })
+
+    } catch (error) {
+      console.error('error in cart data submitting: ', error)
+    }
+  };
+
   return (
     <>
       <Hero />
       <Container centerContent mt={5}>
         <SimpleGrid columns={{ base: 1, sm: 2, md: 3, lg: 5 }} gap={6}>
-          {products.map((product) => 
-            <Card.Root maxW="xs" key={product.id}>
+          {products.map((product) => (
+            <Card.Root
+              as={"form"}
+              onSubmit={(e) => handleSubmit(e, product.id)}
+              maxW="xs"
+              key={product.id}
+            >
               <Image
                 src={`${URL}${product.img}`}
                 alt={`${URL}${product.img}`}
@@ -68,10 +105,23 @@ export default function Home() {
                 </Text>
               </Card.Body>
               <Card.Footer alignSelf={"center"}>
-                <Button variant="solid">Add to cart</Button>
+                <Button type="submit" variant="solid" size="xs">
+                  Add to cart
+                </Button>
+
+                <NumberInput.Root
+                  value={(quantities[product.id] || 1).toString()}
+                  onValueChange={(details) => handleFormChange(product.id, details)}
+                  size={"xs"}
+                  min={1}
+                >
+                  <NumberInput.Control />
+                  <NumberInput.Input name="quantity"/>
+                </NumberInput.Root>
+
               </Card.Footer>
             </Card.Root>
-          )}
+          ))}
         </SimpleGrid>
       </Container>
     </>
